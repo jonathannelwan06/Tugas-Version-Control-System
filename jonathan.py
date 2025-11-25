1be1c6d22dcd313d85a0c2b6f96e6133347764e9
jonathannelwan06 <jonathannelwan06@gmail.com>
2025-11-25 11:52:57 +0800

    Add game over and replay confirmation logic
    Menambahkan kondisi untuk mengecek apakah kesempatan pemain sudah habis dan tebakan pemain masih salah. Jika benar, maka pemain dinyatakan gagal.

--- jonathan.py
+++ jonathan.py
@@ -1,1 +1,15 @@
+# jika kesempatan habis
+    if kesempatan == 0 and tebak != bilRandom:
+        print("Yah gagal tebak angka... :(")
+        print("Jawabannya adalah:", bilRandom)
 
+    # konfirmasi ulang
+    ulang = input("Mau main lagi? (y/n): ").lower()
+    while ulang not in ["y", "n"]:
+        ulang = input("\nMaaf inputan anda salah.\nMau main lagi? (y/n): ").lower()
+
+    if ulang == "y":
+        print("\nSilakan main lagi!!\n")
+    else:
+        print("\nTerima kasih sudah bermain. Sampai jumpa lagi!!\n")
+        break
